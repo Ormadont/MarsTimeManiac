b1d6855132c6e8232540b5e16c8d3fdf178b785a
Демьянов Евгений Павлович <demyanov_ep@OIASUP-DEMYANOV>
2019-03-27 13:42:55 +0400

    Устранена ошибка работы с журалом за прошлый год

--- MarsTimeManiac/Form1.cs
+++ MarsTimeManiac/Form1.cs
@@ -21,14 +21,28 @@ namespace MarsTimeManiac
         {
             SysEventLog sysLog = new SysEventLog();
 
-            string todayTime = sysLog.ChoiceDayTime(DateTime.Now.DayOfYear).ToString();
-            label_TodayTime.Text = $"Сегодня компьютер начал работать {todayTime} назад.";
+            string todayTime = sysLog.TodayTime().Hours.ToString();
+            label_TodayTime.Text = $"Сегодня компьютер начал работать {todayTime}ч. назад., без учёта обеда";
 
-            string yesterdayTime = sysLog.ChoiceDayTime(DateTime.Now.DayOfYear-1).ToString();
+            string yesterdayTime = sysLog.ChoiceDayTime(DateTime.Now.DayOfYear - 1).ToString();
             label_YesterdayTime.Text = $"Вчерашний интервал работы компьютера {yesterdayTime}";
 
-            string monthTime = sysLog.CurMonthTime().ToString();
-            label_monthTime.Text = $"Время с начала месяца до сегодняшнего дня: {monthTime}";
+            int daysCount = 0;
+
+            TimeSpan monthTime = sysLog.CurMonthTime(out daysCount);
+            string monthTime_str = monthTime.ToString();
+
+            // расчётное количество часов за март
+            double planHours = daysCount * 8 - 1;
+
+            // фактическое число часов за месяц
+            double factHours = monthTime.TotalHours;
+
+            label_monthTime.Text = 
+$@"Время с начала месяца до сегодняшнего дня: {monthTime}, 
+всего дней отработано: {daysCount}, 
+не хватает часов: {planHours-factHours}, план:{planHours} факт:{factHours}
+(данные за месяц c учётом семичасового дня, с вычетом обеденного времени и 10 минут)";
         }
     }
 }
--- MarsTimeManiac/SysEventLog.cs
+++ MarsTimeManiac/SysEventLog.cs
@@ -11,13 +11,14 @@ namespace MarsTimeManiac
     {
         EventLog sysLog;
         IEnumerable<EventLogEntry> Entries;
+        int curYear = DateTime.Now.Year;
 
         public SysEventLog()
         {
             sysLog = new EventLog();
             sysLog.Log = "System";
             Entries = sysLog.Entries.Cast<EventLogEntry>();
-            DateTime nowTime = DateTime.Now;
+            //DateTime nowTime = DateTime.Now;
         }
 
         // сколько сегодня прошло времени с момента включения компьютера
@@ -26,58 +27,74 @@ namespace MarsTimeManiac
             // время первого события текущего дня
             DateTime startTime =
                 (from entry in Entries
-                where entry.TimeGenerated.DayOfYear == DateTime.Now.DayOfYear // только события сегодняшнего дня              
-                select entry.TimeGenerated)
+                 where entry.TimeGenerated.DayOfYear == DateTime.Now.DayOfYear // только события сегодняшнего дня
+                 && entry.TimeGenerated.Year == curYear
+                 select entry.TimeGenerated)
                 .Min();
 
             return DateTime.Now - startTime;
         }
 
-        // сколько времени прошло вчера между первым и последним событиями журнала
-        public TimeSpan YesterdayTime()
-        {
-            // время первого события вчерашнего дня
-            DateTime startTime =
-                (from entry in Entries
-                 where entry.TimeGenerated.DayOfYear == DateTime.Now.DayOfYear-1 // только события вчерашнего дня              
-                 select entry.TimeGenerated)
-                .Min();
-            // время последнего события вчерашнего дня
-            DateTime endTime =
-                (from entry in Entries
-                 where entry.TimeGenerated.DayOfYear == DateTime.Now.DayOfYear - 1 // только события вчерашнего дня              
-                 select entry.TimeGenerated)
-                .Max();
+        //// сколько времени прошло вчера между первым и последним событиями журнала
+        //public TimeSpan YesterdayTime()
+        //{
+        //    // время первого события вчерашнего дня
+        //    DateTime startTime =
+        //        (from entry in Entries
+        //         where entry.TimeGenerated.DayOfYear == DateTime.Now.DayOfYear-1 // только события вчерашнего дня              
+        //         select entry.TimeGenerated)
+        //        .Min();
+        //    // время последнего события вчерашнего дня
+        //    DateTime endTime =
+        //        (from entry in Entries
+        //         where entry.TimeGenerated.DayOfYear == DateTime.Now.DayOfYear - 1 // только события вчерашнего дня              
+        //         select entry.TimeGenerated)
+        //        .Max();
 
-            return endTime - startTime;
-        }
+        //    return endTime - startTime;
+        //}
 
         // сколько времени прошло в заданный день между первым и последним событиями журнала
         public TimeSpan ChoiceDayTime(int DayOfYear)
         {
-            // время первого события 
-            DateTime startTime =
-                (from entry in Entries
-                 where entry.TimeGenerated.DayOfYear == DayOfYear // только события указанного дня              
-                 select entry.TimeGenerated)
-                .Min();
-            // время последнего события 
-            DateTime endTime =
-                (from entry in Entries
-                 where entry.TimeGenerated.DayOfYear == DayOfYear // только события указанного дня              
-                 select entry.TimeGenerated)
-                .Max();
+            DateTime startTime = DateTime.Now;
+            DateTime endTime = DateTime.Now;
+            
+            try
+            {
+                // время первого события 
+                startTime =
+                    (from entry in Entries
+                     where entry.TimeGenerated.DayOfYear == DayOfYear // только события указанного дня  
+                     && entry.TimeGenerated.Year == curYear
+                     select entry.TimeGenerated)
+                    .Min();
+                // время последнего события 
+                endTime =
+                    (from entry in Entries
+                     where entry.TimeGenerated.DayOfYear == DayOfYear // только события указанного дня
+                     && entry.TimeGenerated.Year == curYear
+                     select entry.TimeGenerated)
+                    .Max();
+            }
+            catch (Exception)
+            {
+                return TimeSpan.Zero;
+            }
 
-            return endTime - startTime;
+            return endTime - startTime - TimeSpan.FromHours(1.0) - TimeSpan.FromMinutes(10.0); // отнимаем час как обед и 10 минут за гибкий график
         }
 
         // количество времени за текущий месяц
-        public TimeSpan CurMonthTime()
-        {           
+        public TimeSpan CurMonthTime( out int daysCount)
+        {
+            daysCount = 0;
             // время первого события текущего месяца
              DateTime startTime =
                 (from entry in Entries
-                 where entry.TimeGenerated.Month == DateTime.Now.Month // только события вчерашнего дня              
+                 where 
+                    (entry.TimeGenerated.Month == DateTime.Now.Month) // только события вчерашнего дня
+                    && (entry.TimeGenerated.Year == curYear)
                  select entry.TimeGenerated)
                 .Min();
 
@@ -87,10 +104,12 @@ namespace MarsTimeManiac
             // сложение времени с начала месяца по вчерашний день
             TimeSpan sumTimeOfMonth = TimeSpan.Zero;
             
-            for (int i = firstDayOfMonthInYear; i < DateTime.Now.DayOfYear -1; i++)
+            for (int i = firstDayOfMonthInYear; i < DateTime.Now.DayOfYear; i++)
             {
-                sumTimeOfMonth += ChoiceDayTime(i);
-                Debug.WriteLine($@"Номер дня: {i}, Общее подсчитанное время: {sumTimeOfMonth.ToString()}");
+                TimeSpan curTime = ChoiceDayTime(i);
+                if (curTime != TimeSpan.Zero) daysCount++;
+                sumTimeOfMonth += curTime;
+                Debug.WriteLine($@"Номер дня: {daysCount}, Общее подсчитанное время: {sumTimeOfMonth.ToString()}");
             }
 
             Debug.WriteLine($"Общее время за этот месяц: {sumTimeOfMonth.ToString()}");
